7bc10b9e7007589b036d31836dabec86987e53f5
Tiago Mota <tiagomota@tiagosmpalithya.home>
2025-03-24 12:57:24 -0400

    Root group and base layout

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'ProStore';
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'A modern e-commerce platform build with NextJS.';
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
