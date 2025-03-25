bb341c930c3729fec64dc8ed4cc6d9b09480aa01
Tiago Mota <tiagomota@tiagosmpalithya.home>
2025-03-25 11:00:06 -0400

    Fetch data from db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,12 @@
 import ProductList from '@/components/shared/product/product-list';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 
-import sampleData from '@/db/sample-data';
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
 
-const HomePage = () => {
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title='Newest Arrivals'
-        limit={4}
-      />
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
     </>
   );
 };
