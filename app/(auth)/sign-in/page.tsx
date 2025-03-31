ef9a0a2cd02f6cc035976a750f099ca8f4976402
Tiago Mota <tiagomot@gmail.com>
2025-03-31 09:43:57 -0400

    Started populating sign in form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import { APP_NAME } from '@/lib/constants';
 import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import CredentialsSignInForm from './credentials-signin-form';
 
 export const metadata: Metadata = {
   title: 'Sign In',
@@ -33,7 +34,9 @@ const SignInPage = () => {
             Sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className='space-y-4'>{/* form here */}</CardContent>
+        <CardContent className='space-y-4'>
+          <CredentialsSignInForm />
+        </CardContent>
       </Card>
     </div>
   );
