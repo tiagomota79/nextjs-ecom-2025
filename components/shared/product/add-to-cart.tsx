0cbc53a4d0a2233dd703aca88027e9734f902f6c
tiagomota79 <tiagomot@gmail.com>
2025-04-08 15:11:59 -0400

    Transition animation on button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,53 +1,59 @@
 'use client';
+import { useTransition } from 'react';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
-import { Plus, Minus } from 'lucide-react';
+import { Plus, Minus, Loader } from 'lucide-react';
 import { Cart, CartItem } from '@/types';
 import { toast } from 'sonner';
 import { addItemToCard, removeItemFromCart } from '@/lib/actions/cart.actions';
 
 const AddToCart = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleAddToCart = async () => {
-    const response = await addItemToCard(item);
+    startTransition(async () => {
+      const response = await addItemToCard(item);
 
-    if (!response?.success) {
-      toast.error(response?.message, {
-        style: { backgroundColor: 'red', color: 'white' },
+      if (!response?.success) {
+        toast.error(response?.message, {
+          style: { backgroundColor: 'red', color: 'white' },
+        });
+        return;
+      }
+
+      toast.success(response.message, {
+        action: { label: 'View Cart', onClick: () => router.push('/cart') },
+        style: { backgroundColor: 'green', color: 'white' },
+        actionButtonStyle: {
+          backgroundColor: 'white',
+          color: 'green',
+        },
       });
-      return;
-    }
-
-    toast.success(response.message, {
-      action: { label: 'View Cart', onClick: () => router.push('/cart') },
-      style: { backgroundColor: 'green', color: 'white' },
-      actionButtonStyle: {
-        backgroundColor: 'white',
-        color: 'green',
-      },
     });
   };
 
   const handleRemoveFromCart = async () => {
-    const response = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const response = await removeItemFromCart(item.productId);
 
-    if (!response?.success) {
-      toast.error(response?.message, {
-        style: { backgroundColor: 'red', color: 'white' },
+      if (!response?.success) {
+        toast.error(response?.message, {
+          style: { backgroundColor: 'red', color: 'white' },
+        });
+      }
+
+      toast.success(response.message, {
+        action: { label: 'View Cart', onClick: () => router.push('/cart') },
+        style: { backgroundColor: 'green', color: 'white' },
+        actionButtonStyle: {
+          backgroundColor: 'white',
+          color: 'green',
+        },
       });
-    }
 
-    toast.success(response.message, {
-      action: { label: 'View Cart', onClick: () => router.push('/cart') },
-      style: { backgroundColor: 'green', color: 'white' },
-      actionButtonStyle: {
-        backgroundColor: 'white',
-        color: 'green',
-      },
+      return;
     });
-
-    return;
   };
 
   // Check if item is already in cart
@@ -56,18 +62,26 @@ const AddToCart = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
     cart.items.find((cartItem) => cartItem.productId === item.productId);
 
   return existingItem ? (
-    <div className='flex gap-2 items-center'>
+    <div className='flex gap-2 justify-evenly items-center'>
       <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
-        <Minus className='h-4 w-4' />
+        {isPending ? (
+          <Loader className='h-4 w-4 animate-spin' />
+        ) : (
+          <Minus className='h-4 w-4' />
+        )}
       </Button>
       <span className='px-2'>{existingItem.qty}</span>
       <Button type='button' variant='outline' onClick={handleAddToCart}>
-        <Plus className='h-4 w-4' />
+        {isPending ? (
+          <Loader className='h-4 w-4 animate-spin' />
+        ) : (
+          <Plus className='h-4 w-4' />
+        )}
       </Button>
     </div>
   ) : (
     <Button className='w-full' type='button' onClick={handleAddToCart}>
-      <Plus />
+      {isPending ? <Loader className='h-4 w-4 animate-spin' /> : <Plus />}
       Add to Cart
     </Button>
   );
