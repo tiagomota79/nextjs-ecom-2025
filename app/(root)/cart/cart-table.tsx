829aab2e36c08f7f094203350d9f041a9e60a4d5
tiagomota79 <tiagomot@gmail.com>
2025-04-10 09:28:44 -0400

    Cart page base

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { useTransition } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+import { addItemToCard, removeItemFromCart } from '@/lib/actions/cart.actions';
+import { ArrowRight, Loader, Minus, Plus } from 'lucide-react';
+import { Cart } from '@/types';
+
+const CartTable = ({ cart }: { cart?: Cart }) => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <>
+      <h1 className='py-4 h2-bold'>Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href='/'>Go shopping</Link>
+        </div>
+      ) : (
+        <div className='grid md:grid-cols-4 md:gap-5'>
+          <div className='overflow-x-auto md:col-span-3'>Table</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CartTable;
